6d584be5d3e5037d70409b66196a3841384dc5fc
Sysy's <sysys.iwi@gmail.com>
2026-04-13 15:06:14 +0200

    Add locked on not implemented games

--- src/main/java/com/meow/meow/MainMenuController.java
+++ src/main/java/com/meow/meow/MainMenuController.java
@@ -1,6 +1,7 @@
 package com.meow.meow;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
@@ -34,6 +35,15 @@ public class MainMenuController {
         Stage owner = (Stage) blackjackCard.getScene().getWindow();
         BlackjackGameWindow gameWindow = new BlackjackGameWindow();
         gameWindow.show(owner, this::refreshBalance);
+    }
+
+    @FXML
+    private void handleComingSoonClick() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Comming Soon");
+        alert.setHeaderText(null);
+        alert.setContentText("Comming Soon");
+        alert.showAndWait();
     }
 
     private void refreshBalance() {
